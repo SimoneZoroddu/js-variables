3da4cf972fc3ac43543ce4368dee75b2b87732fe
SimoneZoroddu <lenviur@gmail.com>
2026-01-30 12:39:35 +0100

    second exercise second step

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,13 +1,21 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-const name = 'Simone'
+
+const name = 'Simone';
 console.log(name);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+
+const number = 8;
+const text = 'testo';
+const bool = true;
+console.log(number);
+console.log(text);
+console.log(bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
